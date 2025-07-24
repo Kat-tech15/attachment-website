3438cdac77d1c5ed37dca5fb5c8e35c50cf24f6e
Kat-tech15 <kelvinkatwai@gmail.com>
2025-07-24 18:41:33 +0300

    modified models

--- attachment/models.py
+++ attachment/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from phonenumber_field.modelfields import PhoneNumberField
