cc1b53ad42721c513394d78d5f2588bb536ab2d3
Kat-tech15 <kelvinkatwai@gmail.com>
2025-09-05 20:01:56 +0100

    modified settings

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for attachment_website project.
+WSGI config for core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attachment_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
